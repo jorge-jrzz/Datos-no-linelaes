afddc100c148b017a7e181c90ef7da22396699f4
Jorge Juarez <jorgeang33@gmail.com>
2023-03-08 10:31:11 -0600

    add MainPar.java

---
+++ Examenes/Examen_JorgeJuarez/src/MainPar.java
@@ -0,0 +1,11 @@
+public class MainPar {
+    public static void main(String[] args) {
+        Par<Integer, String> p1 = new Par<Integer, String>(7, "hola");
+        Par<Integer, String> p2 = new Par<Integer, String>(5, "dos");
+
+        System.out.println(p1);
+        System.out.println("----------------------");
+        System.out.println(p2);
+    }
+    
+}
